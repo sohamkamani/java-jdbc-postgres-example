ea0654192d4d52e73eb8ded5685a61aad2dea75d
sohamkamani <sohamkamani@gmail.com>
2022-08-27 11:37:29 +0530

    working example

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# {TITLE}
+# Java JDBC Postgres Example
 
-This is the example repo for my blog post on [{TITLE}]({URL})
+This is the example repo for my blog post on [Java JDBC Postgres Example](java/jdbc-postgres)
 
 To run this example:
 
 1. Make sure you have Java, JDK, and Maven installed
 2. Clone the repo
 3. Run `mvn clean compile assembly:single` - this should give you a JAR file in the `target` folder
-4. Run `java -jar target/{PROJECTDIR}-1.0-SNAPSHOT-jar-with-dependencies.jar` to run the JAR file
+4. Run `java -jar target/java-jdbc-postgres-example-1.0-SNAPSHOT-jar-with-dependencies.jar` to run the JAR file
--- src/main/java/com/sohamkamani/App.java
+++ src/main/java/com/sohamkamani/App.java
@@ -1,20 +1,66 @@
 package com.sohamkamani;
 
-import com.github.ricksbrown.cowsay.Cowsay;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import org.postgresql.ds.PGSimpleDataSource;
 
 /**
  * Hello world!
  *
  */
 public class App {
-    public static void main(String[] args) {
-        int result = Calculator.add(4, 5);
-        String[] cowArgs = new String[] { String.valueOf(result) };
-        String cowString = Cowsay.say(cowArgs);
-        System.out.printf(cowString);
+    public static void main(String[] args) throws Exception {
+
+        DataSource dataSource = createDataSource();
+        Connection conn = dataSource.getConnection();
+
+        getAllBirds(conn);
+        getFilteredBirds(conn);
+        insertBird(conn);
+
     }
 
-    public static int add(int n1, int n2) {
-        return n1 + n2;
+    private static DataSource createDataSource() {
+        final String url =
+                "jdbc:postgresql://localhost:5432/bird_encyclopedia?user=dbadmin&password=my-secret-password";
+        final PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        dataSource.setUrl(url);
+        return dataSource;
     }
+
+    // Getting all entries from database
+    private static void getAllBirds(Connection conn) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("select * from birds");
+        ResultSet rs = stmt.executeQuery();
+        while (rs.next()) {
+            System.out.printf("id:%d bird:%s description:%s%n", rs.getLong("id"),
+                    rs.getString("bird"), rs.getString("description"));
+        }
+    }
+
+    // Getting filtered entries from database using query params
+    private static void getFilteredBirds(Connection conn) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("select * from birds where bird=?");
+        stmt.setString(1, "eagle");
+        ResultSet rs = stmt.executeQuery();
+        while (rs.next()) {
+            System.out.printf("id:%d bird:%s description:%s%n", rs.getLong("id"),
+                    rs.getString("bird"), rs.getString("description"));
+        }
+    }
+
+    // add a new bird to the table
+    private static void insertBird(Connection conn) throws SQLException {
+        PreparedStatement insertStmt =
+                conn.prepareStatement("INSERT INTO birds(bird, description) VALUES (?, ?)");
+        insertStmt.setString(1, "rooster");
+        insertStmt.setString(2, "wakes you up in the morning");
+        int insertedRows = insertStmt.executeUpdate();
+        System.out.printf("inserted %s bird(s)%n", insertedRows);
+    }
+
+
 }
